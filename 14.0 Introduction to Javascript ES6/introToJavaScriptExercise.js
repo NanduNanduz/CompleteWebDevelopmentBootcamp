1533cc31e38dc121626c5396b213da7b0ec615dc
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2023-12-06 17:47:11 +0530

    Updated intoToJvaScriptExercise

--- 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
+++ 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
@@ -1,12 +1,15 @@
 // 1.Create the first character to uppercase in a paragraph.
       //a. Create a var that store the name that user enters via promt.
       //b. Capitalise the first letter of their name .
-      //c. We use the capitalized version of their name to greet them using an alert.
+      //c. rest of the charecter should lowercase.
+      //d. We use the capitalized version of their name to greet them using an alert.
 //solution :
       var name = prompt("What is your name?");
       var firstChar = name.slice(0,1);
       var upperCaseFirstChar = firstChar.toUpperCase();
       var restOfName = name.slice(1,name.length);
+      restOfName = restOfName.toLowerCase();
       var capitalizedName = upperCaseFirstChar + restOfName;
-      alert("Hai "+ capitalizedName);
+      alert("Hai, "+ capitalizedName);
+
 
