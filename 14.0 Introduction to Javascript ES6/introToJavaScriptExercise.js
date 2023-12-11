ea241ffab6a3a00a902e420a6b6ac192161357d0
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2023-12-11 22:12:18 +0530

    Life in Weeks Coding Exercise

--- 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
+++ 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
@@ -63,3 +63,36 @@ function beepersLeft() {
   putBeeper();
   move();
 }
+
+// In this challenge, you are going to create a function that tells us how many days, weeks and months we have left if we live until 90 years old.
+// It will take your current age as the input and console.logs a message with our time left in this format:
+// You have x days, y weeks, and z months left.
+// Where x, y and z are replaced with the actual calculated numbers.
+// For this challenge, assume there are 365 days in a year, 52 weeks in a year and 12 months in a year.
+
+
+function lifeInWeeks(age) {
+  /************Don't change the code above************/
+
+  //Write your code here.
+  var remainingYear = 90 - age;
+  var remainingDays = remainingYear * 365;
+  var remainingWeeks = remainingYear * 52;
+  var remainingMonths = remainingYear * 12;
+
+  console.log(
+    "You have " +
+      remainingDays +
+      " days, " +
+      remainingWeeks +
+      " weeks, and " +
+      remainingMonths +
+      " months left."
+  );
+  /*************Don't change the code below**********/
+}
+
+lifeInWeeks(19);
+
+
+
