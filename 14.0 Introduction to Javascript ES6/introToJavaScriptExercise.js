2d66f364f80f2a68bd888727fcc368be7562751e
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2023-12-13 20:44:19 +0530

    created BMI Calculator using JavaScript

--- 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
+++ 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
@@ -92,3 +92,27 @@ function lifeInWeeks(age) {
 }
 
 lifeInWeeks(19);
+
+
+
+//Creating BMI Calculator
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+function bmiCalculator(x,y)
+{
+    
+    var value = x/(y*y);
+    return value;
+    
+}
+var bmi = bmiCalculator(65, 1.8); 
+console.log(bmi);
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
+
