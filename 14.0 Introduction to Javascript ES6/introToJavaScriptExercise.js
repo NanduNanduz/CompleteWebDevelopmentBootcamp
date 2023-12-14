244f264a999ad4085f900d32551b542955fa8ed4
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2023-12-14 22:53:45 +0530

    updated Intro to JavaScript

--- 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
+++ 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
@@ -93,20 +93,15 @@ function lifeInWeeks(age) {
 
 lifeInWeeks(19);
 
-
-
 //Creating BMI Calculator
 //Create your function below this line.
 //The first parameter should be the weight and the second should be the height.
 
-function bmiCalculator(x,y)
-{
-    
-    var value = x/(y*y);
-    return value;
-    
+function bmiCalculator(x, y) {
+  var value = x / (y * y);
+  return value;
 }
-var bmi = bmiCalculator(65, 1.8); 
+var bmi = bmiCalculator(65, 1.8);
 console.log(bmi);
 /* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
 
@@ -115,4 +110,3 @@ var bmi = bmiCalculator(65, 1.8);
 bmi should equal 20 when it's rounded to the nearest whole number.
 
 */
-
