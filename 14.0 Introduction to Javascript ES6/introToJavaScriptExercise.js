6d51349207391ee4ad72c38661660bf6442594d4
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-01-20 00:29:12 +0530

    working with js array : intro to js

--- 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
+++ 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
@@ -149,3 +149,30 @@ function isLeap(year) {
   /**************Don't change the code below****************/
 }
 isLeap(2024);
+
+
+
+// ---------------------------------------- Exercise 6: working with js array--------------------------------------------------------------
+
+
+var output = [];
+var count = 1;
+function fizzBuzz(){
+   if(count % 3 === 0 && count % 5 === 0){
+       output.push("fizzBuzz");
+   }
+    else if(count % 3 === 0){
+        output.push("fizz");
+    }
+    else if(count % 5 === 0){
+            output.push("buzz");
+    }
+    else{
+        output.push(count);
+    }
+    count++;
+    console.log(output);
+}
+ 
+
+
