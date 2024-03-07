88ec4eb776dc49d95b70acfc27fd960385b146ab
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-03-07 23:17:35 +0530

    Fibonacci Solution exercise

--- 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
+++ 14.0 Introduction to Javascript ES6/introToJavaScriptExercise.js
@@ -176,3 +176,23 @@ function whosPaying(names) {
   var randomPerson = names[position];
   return randomPerson + " is going to buy lunch today!";
 }
+
+//----------------------------------------- Exercise 8: Fibonacci Solution ------------------------------------------------
+
+
+function fibonacciGenerator(n) {
+  var output = [];
+  if (n === 1) {
+    output = [0];
+  } else if (n === 2) {
+    output = [0, 1];
+  } else {
+    output = [0, 1];
+    for (var i = 2; i < n; i++) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
+    }
+  }
+  return output;
+}
+output = fibonacciGenerator(5);
+console.log(output);
