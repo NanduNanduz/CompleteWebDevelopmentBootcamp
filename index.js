1cb248a54fbd9b8d1903d9c1e8e94d4f02e75b39
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-06-20 23:32:28 +0530

    High Order Function Challenge  completed

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // Calculator 
 function add(num1, num2) {
-  return num1 + num2;
+  return num1+num2;
+}
+
+function substract(num1, num2){
+    return num1-num2;
 }
 
 function multiply(num1, num2) {
-  return num1 + num2;
+  return num1+num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
 }
 
 function calculator(num1, num2, operator) {
