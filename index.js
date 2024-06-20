3cefaa2103d9a96437d4b0d8a57e9790e0ea4e54
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-06-20 23:20:56 +0530

    updated with High Order Function

---
+++ index.js
@@ -0,0 +1,16 @@
+// Calculator 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 + num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
+
+calculator(1, 2, add);
+
+//-----> High Order Functions - are fun that can  take  other function as its input.
