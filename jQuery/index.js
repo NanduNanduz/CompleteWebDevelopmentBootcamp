761952ba6729face765fad72fb37dca258fbcf3f
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-09-29 22:58:02 +0530

    adding eventListeners with jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,5 +1,10 @@
 
-$("h1").addClass("big-title margin-50");
-$("h1").text("hai")
+//$("h1").addClass("big-title margin-50");
+//$("h1").text("hai")
+//$("h1").click(function(){
+  //  $("h1").css("color", "purple");
+//});
+$("button").click(function(){
+    $("h1").css("color","purple");
 
-
+})
