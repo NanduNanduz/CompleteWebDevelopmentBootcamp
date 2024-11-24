73bfc95b58292bbcf3eab34ebdba1826abb09f05
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-11-24 14:39:21 +0530

    rearranged folders for to use the package sillyname

---
+++ Backend/NPM/index.js
@@ -0,0 +1,16 @@
+// const fs = require("fs");
+
+// fs.writeFile("message.txt", "Hello from Node.js!", (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// });
+
+// fs.readFile("./message.txt", "utf8",(err, data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+
+var generateName = require("sillyname");
+var sillyName = generateName();
+console.log('My name is ',sillyName);
